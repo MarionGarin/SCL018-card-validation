a8b84d5151eaa0c0182adaff30f41e724e96c858
mariongarin <mariongarino@gmail.com>
2021-09-27 18:30:51 -0300

    Versión Semifinal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+   document.getElementById("validate").addEventListener("click", checkIn); /*Capturador que activa initialCheckIn*/
+
+   function checkIn() {/*verifica el contenido ingresado y decide qué hacer con él a través de ifs y elses*/
+  
+     const boxContent = document.getElementById("creditCardNumber").value; /*captura el valor del input*/
+     let contentToValidate = boxContent.toString(); /*Convierte el número del input en String, para aplicar método match*/
+
+     if (contentToValidate.match("^[0-9]{16}$")) { /*Match compara el string con un número entero de 16 dígitos*/
+         alert("Estamos procesando tu solicitud"); 
+
+           if (validator.isValid(contentToValidate)) {
+           alert("Tu tarjeta N°  " + validator.maskify(contentToValidate) + " se ha registrado exitosamente"
+           );
+
+          } else {
+           alert("Oops... el número ingresado no es válido; por favor intenta nuevamente");
+           }
+
+      } else {
+        alert("Debes ingresar un número de 16 dígitos");
+        throw TypeError("Only numbers allowed");
+
+     }
+
+  }
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,80 @@
+
 const validator = {
-  // ...
+  
+    isValid(contentToValidate){
+
+        let digitsForLuhn = contentToValidate.split ("").reverse(); /*se forma un array y se revierten los números*/
+                              
+        let evenPosition = []; /*array con posiciones pares*/
+        let oddPosition = []; /*array con posiciones impares*/
+
+            for (let i = 0; i < digitsForLuhn.length;i++){ 
+                 if ((i+1) % 2 === 0){
+                 evenPosition.push(digitsForLuhn[i]);
+              
+                } else {
+                 oddPosition.push(digitsForLuhn[i]);
+                }
+
+             }
+            
+        let duplicatedDigits = []; /*array con los números de posiciones pares duplicados*/
+            evenPosition.forEach (item => {
+            duplicatedDigits.push(item*2);
+            });
+        
+        let twoDigits = duplicatedDigits.filter(item => item > 9);//array con números de 2 dígitos, con comprobación ">9"
+        let oneDigit = duplicatedDigits.filter(item => item <= 9);//eligiendo números de 1 dígitos, con comprobación "<=9"
+
+        let twoDigitsSumed = []; //array con la suma de los dígitos dobles, con método indirecto de restar 9
+            twoDigits.forEach (item => {
+            twoDigitsSumed.push(item-9);
+            });
+
+        let allToIntegers = (twoDigitsSumed.concat(oneDigit,oddPosition)).map(Number); //se convierte todo a enteros
+       
+        let totalSum=0; //suma de todos los números para aplicar módulo de 10
+
+        for(let i of allToIntegers) totalSum+=i; /*función que aplica comprobación final y retorna booleans*/
+            if (totalSum % 10 === 0) { 
+            return true; 
+
+        } else {
+            return false;
+            
+          }
+            
+        },
+            
+                     
+    maskify (contentToValidate){
+
+        return contentToValidate.slice(0,-3).replace(/.(?=.)/g, '#').concat(contentToValidate.slice(-3, contentToValidate.length));
+        
+    }
+
+    
+
+    
 };
 
 export default validator;
+
+
+
+
+
+
+//Seleccionar los números pares, ok
+
+//Multiplicar los números pares por 2, ok
+
+//Sumar todos los dígitos que resultaron ser dobles (a todos los >9 restar 9 y eso equivale a la suma de 2 dígitos), ok
+
+//Sumar todos los dígitos del array, 
+
+//Dividir el resultado por 10
+
+//Evaluar el residuo
+
+//Puede que use array.join() o array.filter()
